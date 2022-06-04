dd52aa8de17f75edb4abf24feb110af3da5cc3b9
Primetraineesvn <primetraineesvn@gmail.com>
2022-06-04 18:08:25 +0100

    Student-app-serenity project Today's Update

--- src/main/java/com/studentapp/constants/EndPoints.java
+++ src/main/java/com/studentapp/constants/EndPoints.java
@@ -10,7 +10,7 @@ public class EndPoints {
      */
     public static final String GET_ALL_STUDENT = "/list";
     public static final String GET_SINGLE_STUDENT_BY_ID = "/{studentID}";
-    public static final String UPDATE_STUDENT_BY_ID = "/{studentID}.json";
+    public static final String UPDATE_STUDENT_BY_ID = "/{studentID}";
     public static final String DELETE_STUDENT_BY_ID = "/{studentID}";
 
     /**
